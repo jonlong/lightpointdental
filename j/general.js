b57888ec3f1f731910045dbf612e5b2758d85b75
Jon Long <jon@linesandwaves.com>
2017-03-07 15:16:59 -0500

    first commit

---
+++ j/general.js
@@ -0,0 +1,20 @@
+// ===================================================    
+//*=== Activate JS-enabled class immediately  ===*
+// ===================================================
+
+$('html').removeClass('nojs').addClass('js');
+
+// ===================================================    
+//*=== Load custom fonts with Google WebFont loader ===*
+// ===================================================
+
+WebFont.load({
+    custom: { families: ['MuseoSlab500'],
+            urls: [ 'http://lightpoint.linesandwaves.com/c/fonts.css'] 
+    }
+});
+
+$(document).ready(function() {
+    
+    
+});
